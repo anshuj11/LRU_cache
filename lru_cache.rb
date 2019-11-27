7be7ada6ab2c983e66eef2ad8c2180016b2255b1
Anshuj <anshuj@gmail.com>
2019-11-27 07:38:38 -0800

    first commit

---
+++ lru_cache.rb
@@ -0,0 +1,72 @@
+class LRUCache
+    def initialize(size)
+         @size=size
+         @cache_keys=[]
+         @cache_values=[]
+    end
+
+    def count
+        print @cache_keys
+        p
+      @cache_keys.count
+    end
+
+    def set(el)
+        ind = @cache_keys.index(el.keys[0])
+       if  ind
+            print "Before slice: ", @cache_keys
+            p
+            @cache_keys = @cache_keys.slice(0...ind).concat( @cache_keys.slice(ind...-1))
+            p
+            print "After slice: ", @cache_keys
+            p
+            @cache_keys << el.keys[0]
+           
+            @cache_values = @cache_values.slice(0...ind).concat(@cache_values.slice(ind...-1))
+            @cache_values << el.values[0]
+        elsif count == @size
+            @cache_keys.shift
+            @cache_values.shift
+            @cache_keys << el.keys[0]
+            @cache_values << el.values[0]
+        else
+            @cache_keys<< el.keys[0]
+            @cache_values << el.values[0]
+        end
+    end
+
+    def show
+      p @cache
+      nil
+    end
+
+    def get(key)
+        ind = @cache_keys.index(key)
+        if ind
+            @cache_values[ind]
+        else
+            nil
+        end    
+    end
+
+    private
+    # helper methods go here!
+
+  end
+
+
+  lru_cache = LRUCache.new(4);
+  lru_cache.set({"test01" => [1,2,3,4]})
+  lru_cache.set({"test02" => { "1"=> [2]}  })
+  lru_cache.set("test03" => 3)
+  lru_cache.set("test04" => 4)
+  lru_cache.set("test05" => 5)
+  lru_cache.set("test03" => 3)
+  
+  p "Count: ", lru_cache.count
+  p lru_cache.get("test01");
+  p lru_cache.get("test02");
+  p lru_cache.get("test03");
+   p lru_cache.get("test04");
+  p "Count: ", lru_cache.count
+
---
+++ lru_cache_2.rb
@@ -0,0 +1,110 @@
+class Node
+    attr_accessor :val, :next, :prev
+    def initialize(val)
+        @val = val
+        @next = nil
+        @prev = nil
+    end
+end
+
+
+class List
+    attr_accessor :head , :tail
+    def initialize
+        @head = nil
+        @tail = nil
+    end
+
+    def insert(el)
+        n = Node.new(el)
+
+        if(@head)
+          @head.next = n
+          n.prev = @head
+        else
+          @head = n  
+        end
+
+        if(@tail)
+            @tail.next = n
+            n.prev = @tail
+            @tail = n
+        else
+            @tail = n    
+        end  
+        n  
+    end
+
+    def delete(ptr)
+        if head == ptr && tail == ptr
+            @head = nil
+            @tail = nil
+            
+        else
+           ptr.prev.next = ptr.next if ptr.prev
+           ptr.next.prev = ptr.prev  if ptr.next
+        end
+        ptr.val
+    end
+
+    def show(ptr)
+     print ptr.val
+     return ptr.val
+    end
+
+end
+
+class Lru_cache
+    attr_accessor :count
+    attr_reader :size
+    def initialize(size)
+        @lru_map = Hash.new()
+        @lru_list = List.new()
+        @size = size
+        @count = 0
+    end
+
+    def insert(el)
+        ptr = @lru_list.insert(el)
+        @lru_map[el.keys[0]] = ptr
+        @count+=1
+        if(@count > size)
+            self.delete()
+        end    
+
+    end
+
+    def delete()
+        
+        el = @lru_list.delete(@lru_list.head)
+        @lru_map.delete(el.keys[0])
+        @count -=1
+    end
+
+    def get(key)
+        ptr = @lru_map[key]
+        if ptr
+          @lru_list.show(ptr)[key]
+        end  
+    end
+end
+
+l = List.new()
+n = l.insert({"1"=>3})
+p n
+l.delete(n)
+#l.show(l.head)
+
+c = Lru_cache.new(5)
+c.insert({"test01"=> [1,2,3,4]})
+#c.get("test01")
+c.insert({ "test02"=>{"1"=> [2]} })
+#c.get("test02")
+c.insert({"test03"=> 3})
+#c.get("test03")
+c.insert({"test04"=>nil})
+#c.get("test04")
+c.insert({"test05"=>"a"})
+print c.get("test05")
+c.insert({"test06"=>{"a"=>[1,2,3,4]}})
+c.get("test01")
